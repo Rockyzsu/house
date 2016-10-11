cd95cd0c9b8fd27620beab78706cf009cf00774e
Rockyzsu <jinweizsu@gmail.com>
2016-10-11 23:35:38 +0800

    完成基本功能

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #-*-coding=utf-8-*-
-__author__ = 'xda'
-
+__author__ = 'rocky'
+#获取每天深圳一手房，二手房的成交套数与面积
 import urllib2,re
 import database
 def getContent():
@@ -14,13 +14,18 @@ def getContent():
     reg=re.compile(r'<td width="14%"><b>(\d+)</b>')
     result=reg.findall(content)
     current_date=date.findall(content)
+
+    reg2=re.compile(r'<td align="right"><b>(.*?)</b>')
+    yishou_area=reg2.findall(content)
+    for i in yishou_area:
+        print i
     '''
     for i in current_date:
         print i
     '''
     print current_date[0]
     print '一手商品房成交套数：%s'  % result[0]
-    print '一手商品房成交面积： %s'  % result[1]
+    print '一手商品房成交面积： %s'  % yishou_area[0]
 
 
     sec_req=urllib2.Request(url+second_hand)
@@ -29,16 +34,11 @@ def getContent():
     #sec_quantity=re.compile(r'<td style="border-color:#DEEFF5;width:30%;">(\d+)</td>')
     sec_quantity=re.compile(r'<td width="30%">(\d+)</td>')
     sec_result=sec_quantity.findall(sec_content)
-    '''
-    k=0
-    for j in sec_result:
-        print k
-        print j
-        k=k+1
-    '''
+    second_area=re.findall(r'<td align="right">(.*?)</td>',sec_content)
+
     print '二手商品房成交套数：%s'  % sec_result[1]
-    print '二手商品房成交面积： %s'  % sec_result[5]
+    print '二手商品房成交面积： %s'  % second_area[2]
     database.create_table()
-    database.insert(current_date[0],result[0],'0',sec_result[1],'0')
+    database.insert(current_date[0],result[0],yishou_area[0],sec_result[1],second_area[2])
 
 getContent()
